09e2561944a15f4f55cabff3749356adaa6ab924
gbzhang6 <guibzhang@gmail.com>
2018-04-04 00:18:24 -0400

    Done.

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -2,20 +2,21 @@ require 'rest-client'
 require 'json'
 require 'pry'
 
-def get_character_movies_from_api(character)
- #make the web request
+def get_character_movies_from_api(character) #make the web request
  char_movies = []
  movies_array = []
- for i in 1..9 do
-   all_characters = RestClient.get("https://www.swapi.co/api/people/?page=" + i.to_s)
-   character_hash = JSON.parse(all_characters)
-   character_hash["results"].each { |character_data|
-     if character_data["name"] == character
-       char_movies = character_data["films"]
-     end
-   }
- end
-
+ unless char_movies.size > 0
+   for i in 1..9 do
+     all_characters = RestClient.get("https://www.swapi.co/api/people/?page=" + i.to_s)
+     #binding.pry
+     character_hash = JSON.parse(all_characters)
+     character_hash["results"].each { |character_data|
+       if character_data["name"] == character
+         char_movies = character_data["films"]
+       end
+     }
+   end
+end
    char_movies.each do |urls|
      all_movies = RestClient.get "#{urls}"
      movies_hash = JSON.parse(all_movies)
@@ -23,20 +24,6 @@ def get_character_movies_from_api(character)
    end
    movies_array
 end
-
-#def character_movies_names(character_movies)
-#  all movies = RestClient.get('http://www.swapi.co/api/films/')
-#  movies_hash = JSON.parse(all_movies)
-#  binding.pry
-#  movie_name = movie_hash[]
-
-#  end
-
-
-#http://www.swapi.co/api/films/#{num}
-
-
-
 
  # iterate over the character hash to find the collection of `films` for the given
  #   `character`
@@ -48,16 +35,14 @@ end
  #  and that method will do some nice presentation stuff: puts out a list
  #  of movies by title. play around with puts out other info about a given film.
 
-def parse_character_movies(films_hash)
-   films_hash.each do |title|
-     puts "Title: #{title}"
-   end
  # some iteration magic and puts out the movies in a nice list
+def parse_character_movies(films_array)
+   films_array.each.with_index(1) { |title, index| puts "#{index}. #{title}" }
 end
 
 def show_character_movies(character)
- films_hash = get_character_movies_from_api(character)
- parse_character_movies(films_hash)
+ films_array = get_character_movies_from_api(character)
+ parse_character_movies(films_array)
 end
 
 
